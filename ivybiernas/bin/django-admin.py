0e6c2b038ffb9170619680dedf288348024fa995
Ira <ivybiernas@addu.edu.ph>
2019-01-21 18:36:56 +0800

    Initial Commit

---
+++ ivybiernas/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ivybiernas/ybiernas1/ivybiernas/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Question
 # Create your views here.
 
 def index(request):
-    return HttpResponse('ANG GANDA MO PO IIRRAA!')
+    context = {}
+    questions = Question.objects.all()
+    context['questions'] = questions
+    return render(request, 'index.html', context)
 
 def help(request):
     return HttpResponse('This is the help page')
